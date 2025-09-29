b8b1da74eb09be120b4510b7b8d28d84028b1cb1
subjen <subbotinjenay@mail.ru>
2025-09-29 19:38:37 +0500

    Add LargeDictionary

---
+++ LargeDictionary_R7/LargeDictionary.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargeDictionary_R7
+{
+    public class LargeDictionary<TKey, TValue> where TKey : notnull
+    {
+        private const int PageSize = 1000000; // 1M элементов на страницу
+        private readonly Dictionary<int, Dictionary<TKey, TValue>> Pages;
+
+        public LargeDictionary() 
+        {
+            Pages = [];
+        }
+
+        public TValue this[TKey key] 
+        {
+            get
+            {
+                if (TryGetValue(key, out TValue value))
+                {
+                    return value;
+                }
+                throw new KeyNotFoundException();
+            }
+            set => Add(key, value);
+        }
+
+        public ICollection<TKey> Keys => throw new NotImplementedException(); //_pages.Values.Select(page => page.Keys);
+
+        public ICollection<TValue> Values => throw new NotImplementedException();
+
+        public long Count => Pages.Values.Sum(page => (long)page.Count);
+
+        public void Add(TKey key, TValue value)
+        {
+            int pageIndex = GetPageIndex(key);
+
+            if (!Pages.TryGetValue(pageIndex, out var page))
+            {
+                page = new Dictionary<TKey, TValue>(PageSize / 1000);
+                Pages[pageIndex] = page;
+            }
+
+            page.Add(key, value);
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        public void Clear()
+        {
+            Pages.Clear();
+        }
+
+
+        public bool ContainsKey(TKey key)
+        {
+            int pageIndex = GetPageIndex(key);
+            if (Pages.TryGetValue(pageIndex, out var page))
+            {
+                return page.ContainsKey(key);
+            }
+            return false;
+        }
+
+
+        public bool Remove(TKey key)
+        {
+            int pageIndex = GetPageIndex(key);
+            if (Pages.TryGetValue(pageIndex, out var page))
+            {
+                return page.Remove(key);
+            }
+            return false;
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return Remove(item.Key);
+        }
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            int pageIndex = GetPageIndex(key);
+            value = default;
+
+            if (Pages.TryGetValue(pageIndex, out var page))
+            {
+                return page.TryGetValue(key, out value);
+            }
+            return false;
+        }
+
+        private static int GetPageIndex(TKey key)
+        {
+            int hashCode = (key.GetHashCode() & 0x7fffffff) / PageSize;
+            return hashCode;
+        }
+    }
+}
--- LargeDictionary_R7/Program.cs
+++ LargeDictionary_R7/Program.cs
@@ -1,2 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LargeDictionary_R7;
+using System.Diagnostics;
+
+var len = int.MaxValue >> 2;
+var sw = new Stopwatch();
+
+
+StartTestDict();        //1m 40 sec - 2m 48 sec / get 00.085 sec
+StartTestLargeDict();   //25-30 sec / get 00.058 sec
+
+void Add(Dictionary<long, long> dict, long len, Stopwatch sw)
+{
+    for (long i = 0; i < len; i++)
+    {
+        dict[i] = i;
+        if (i % 1000000 == 0 && i != 0)
+        {
+            Console.WriteLine($"{i};{sw.Elapsed}");
+        }
+    }
+}
+
+void Get(Dictionary<long, long> dict, long len, Stopwatch sw)
+{
+    for (long i = 1; i * 10000000 < len; i++)
+    {
+        Console.WriteLine($"{dict[i * 10000000]};{sw.Elapsed}");
+    }
+}
+
+void Add2(LargeDictionary<long, long> dict, long len, Stopwatch sw)
+{
+    for (long i = 0; i < len; i++)
+    {
+        dict[i] = i;
+        if (i % 1000000 == 0 && i != 0)
+        {
+            Console.WriteLine($"{i};{sw.Elapsed}");
+        }
+    }
+}
+
+void Get2(LargeDictionary<long, long> dict, long len, Stopwatch sw)
+{
+    for (long i = 1; i * 10000000 < len; i++)
+    {
+        Console.WriteLine($"{dict[i * 10000000]};{sw.Elapsed}");
+    }
+}
+
+void StartTestDict()
+{
+    var dict = new Dictionary<long, long>(210);
+    sw.Start();
+    Add(dict, len, sw);
+    sw.Stop();
+    sw.Restart();
+    Get(dict, len, sw);
+    sw.Stop();
+}
+
+void StartTestLargeDict()
+{
+    var dict = new LargeDictionary<long, long>();
+    sw.Start();
+    Add2(dict, len, sw);
+    sw.Stop();
+    sw.Restart();
+    Get2(dict, len, sw);
+    sw.Stop();
+}
